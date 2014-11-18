27e47762f4dde308d1c0b04b2f83095fd052a708
Joe Teo <revelate@live.com>
2014-11-17 17:21:20 -0800

    travis

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 'use strict';
 
 var express = require('express');
-var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
 var passport = require('passport');
 var app = express();
 
